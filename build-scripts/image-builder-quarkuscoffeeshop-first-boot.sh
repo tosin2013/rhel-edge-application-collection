6986e47fb3735892d98eec4c945a718cd2b285ec
Tosin Akinosho <takinosh@redhat.com>
2022-11-22 21:55:28 -0500

    Update image-builder-quarkuscoffeeshop-first-boot.sh

--- build-scripts/image-builder-quarkuscoffeeshop-first-boot.sh
+++ build-scripts/image-builder-quarkuscoffeeshop-first-boot.sh
@@ -9,23 +9,23 @@ else
    exit 1
 fi 
 
-echo "Running First Boot Script" > /tmp/first-boot.log
+echo "Running First Boot Script" >  /opt/quarkuscoffeeshop-majestic-monolith/first-boot.log
 until [ "`podman inspect -f {{.State.Running}} postgresql-1`"=="true" ]; do
     sleep 0.1;
 done;
 
 sleep 30s 
-echo "Loading database"  | tee -a /tmp/first-boot.log
+echo "Loading database"  | tee -a  /opt/quarkuscoffeeshop-majestic-monolith/first-boot.log
 podman exec -i postgresql-1  /bin/bash -c "PGPASSWORD=${DATABASE_PASSWORD} psql --username ${DATABASE_USER} ${DATABASE_NAME} < /data/init-postgresql.sql" | tee -a /tmp/first-boot.log
 
 until [ "`podman inspect -f {{.State.Running}} quarkuscoffeeshop-majestic-monolith-1`"=="true" ]; do
     sleep 0.1;
 done;
 
-podman stop quarkuscoffeeshop-majestic-monolith-1  | tee -a /tmp/first-boot.log
+podman stop quarkuscoffeeshop-majestic-monolith-1  | tee -a  /opt/quarkuscoffeeshop-majestic-monolith/first-boot.log
 sleep 30s
-echo "Waiting for quarkuscoffeeshop-majestic-monolith-1"  | tee -a /tmp/first-boot.log
+echo "Waiting for quarkuscoffeeshop-majestic-monolith-1"  | tee -a  /opt/quarkuscoffeeshop-majestic-monolith/first-boot.log
 until [ "`podman inspect -f {{.State.Running}} quarkuscoffeeshop-majestic-monolith-1`"=="true" ]; do
     sleep 0.1;
 done;
-podman ps  | tee -a /tmp/first-boot.log
+podman ps  | tee -a  /opt/quarkuscoffeeshop-majestic-monolith/first-boot.log
