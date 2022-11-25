995d0a955c768c7db069cf4a00d099fa27308566
Tosin Akinosho <takinosh@redhat.com>
2022-11-25 11:13:42 -0500

    updating script location

--- build-scripts/image-builder-quarkuscoffeeshop-greenboot.sh
+++ build-scripts/image-builder-quarkuscoffeeshop-greenboot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-
+# Script Location: /etc/greenboot/green.d/image-builder-quarkuscoffeeshop-greenboot.sh
 if [ -f /opt/quarkuscoffeeshop-majestic-monolith/rhel-edge-application-collection/app_env ] ; 
 then
     set -xe 
@@ -16,6 +16,7 @@ if [ $(  sudo podman ps  -a  | grep  postgresql-1 | wc -l)  -eq 1 ]; then
     ${RUN_AS_SUDO} podman stop pgadmin4-1
     ${RUN_AS_SUDO} podman rm pgadmin4-1
     /opt/quarkuscoffeeshop-majestic-monolith/rhel-edge-application-collection/build-scripts/applications/postgresql/postgresql.sh
+    podman exec -i postgresql-1  /bin/bash -c "PGPASSWORD=${DATABASE_PASSWORD} psql --username ${DATABASE_USER} ${DATABASE_NAME} < /data/init-postgresql.sql"
 fi 
 
 
